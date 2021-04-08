6fb1aac6b5aced344eb1af5fc408d51d07d80c8b
Andrey Anosov <andygoalkeeper@gmail.com>
2021-04-09 03:02:19 +0400

    Minifications, new font and renewed favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
+const htmlmin = require('html-minifier');
+const cleanCSS = require('clean-css');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('CNAME');
-  eleventyConfig.addPassthroughCopy('favicon.*');
+  eleventyConfig.addPassthroughCopy('favicon*');
+  eleventyConfig.addPassthroughCopy('robots.txt');
+
+  eleventyConfig.addFilter('cssmin', (code) => {
+    return new cleanCSS().minify(code).styles;
+  });
+
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath && outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
